e7dfcfc615c35cb35bcb37a1eda41227eb9b48e0
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2020-05-07 21:08:01 -0700

    add first characters

---
+++ wizarding_world/characters.py
@@ -0,0 +1,10 @@
+from story_time.characters import Character
+
+
+HARRY_POTTER = Character(first_name='Harry', last_name='Potter')
+RON_WEASLEY = Character(first_name='Ron', last_name='Weasley')
+HERMIONE_GRANGER = Character(first_name='Hermione', last_name='Granger')
+RUBEUS_HAGRID = Character(first_name='Rubeus', last_name="Hagrid")
+
+
+HARRY_POTTER.say('hi')
