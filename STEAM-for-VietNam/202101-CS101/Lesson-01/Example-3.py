e3ef459684928af8900755a6f4a83e3bd437c789
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2021-01-30 19:48:58 -0800

    add Lesson-1 examples

---
+++ STEAM-for-VietNam/202101-CS101/Lesson-01/Example-3.py
@@ -0,0 +1,7 @@
+passcode = input("Xin nhap vao mat khau: ")
+print("Ban da nhap vao: " + passcode)
+
+if passcode == "0042":
+    print("Chao mung ban den voi Sieu May Tinh!")
+else:
+    print("Mat khau sai!")
---
+++ STEAM-for-VietNam/202101-CS101/Lesson-01/Example-4.py
@@ -0,0 +1,9 @@
+# Thuc hanh phep so sanh
+
+print(3 < 4)
+print(3 > 4)
+
+print("Hello" == "hello")
+print("42" == 42)
+
+# print("3" > 4)
