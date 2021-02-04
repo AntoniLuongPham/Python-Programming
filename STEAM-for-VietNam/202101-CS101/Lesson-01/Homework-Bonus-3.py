a801c1a890bfaa0623cc14427010677c08905f1f
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2021-02-03 18:00:31 -0800

    add Homework-Bonus-3

---
+++ STEAM-for-VietNam/202101-CS101/Lesson-01/Homework-Bonus-3.py
@@ -0,0 +1,11 @@
+# Hỏi người chơi “Ban hay dien vao mot con so“.
+# So sánh số đó với 100, nếu số đó lớn hơn 100 thì in ra “Cao hon 100”,
+# nếu không thì in ra “Thap hon hoac bang 100"
+
+number = int(input('Ban hay dien vao mot con so '))
+
+if number > 100:
+    print('Cao hon 100')
+
+else:
+    print('Thap hon hoac bang 100')
