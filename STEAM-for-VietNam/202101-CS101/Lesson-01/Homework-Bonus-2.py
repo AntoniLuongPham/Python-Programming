88447e5e5d2f0f6cf6277bc96cb845855ff78132
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2021-02-03 21:45:50 -0800

    refactor Homework-Bonus-2

--- STEAM-for-VietNam/202101-CS101/Lesson-01/Homework-Bonus-2.py
+++ STEAM-for-VietNam/202101-CS101/Lesson-01/Homework-Bonus-2.py
@@ -1,5 +1,5 @@
 # Viết một chương trình hỏi người chơi “Chao ban, ban ten gi?”
 # và in ra “Chao ban ” cùng với tên của bạn đó.
 
-print('Chao ban, ban ten gi?')
-print('Chao ban Trau')
+ten = input('Chao ban, ban ten gi? ')
+print('Chao ban ' + ten)
