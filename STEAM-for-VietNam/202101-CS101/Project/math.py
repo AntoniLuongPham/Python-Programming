3050bc18fa38ee2b2c0cd8151bb3a0aecc2030b1
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2021-03-13 21:11:54 -0800

    add math tests

---
+++ STEAM-for-VietNam/202101-CS101/Project/math.py
@@ -0,0 +1,12 @@
+
+def find():
+    A = int(input('chose a number. '))
+    B = int(input('chose a number '))
+    if A == B:
+        print('True')
+
+    elif A != B:
+        print('False')
+
+
+find()
