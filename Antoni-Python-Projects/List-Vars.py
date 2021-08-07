c2de84fa315425b16114afc55d3bd4f106303f9c
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2021-08-06 21:37:13 -0700

    add  List-Vars program

---
+++ Antoni-Python-Projects/List-Vars.py
@@ -0,0 +1,24 @@
+one = "1"
+two = "2"
+three = "3"
+four = "4"
+five = "5"
+six = "6"
+seven = "7"
+eight = "8"
+nine = "9"
+ten = "10"
+
+one_to_ten = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
+one_to_ten_vars = [one, two, three, four, five, six, seven, eight, nine, ten]
+print(one_to_ten, '\n', one_to_ten_vars, sep='')
+print(one, two, three, four, five, six, seven, eight, nine, ten)
+
+primary_colors = ["blue", "red", "yellow"]
+secondary_colors = [
+    ["orange", "red and yellow"],
+    ["green", "blue and yellow"],
+    ["purple", "red and blue"]
+]
+
+print(secondary_colors[1])
