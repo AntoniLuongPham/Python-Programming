89cee6f2a41fc90ee7ab7c0df774505337189beb
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2021-08-06 21:52:59 -0700

    add How to Jump

---
+++ Antoni-Python-Projects/How-to-Jump.py
@@ -0,0 +1,9 @@
+how_to_jump = []
+step0 = "stand up. "
+step1 = "bend your knees. "
+step2 = ("tiptoe, push the floor and try to streighten your knees "
+         "while you go up.")
+steps = [step0, step1, step2]
+for step in steps:
+    how_to_jump.append(step)
+print(how_to_jump)
