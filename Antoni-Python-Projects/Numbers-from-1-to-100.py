38d71aee3a3ea8df1998f75e3ce466baf0d7d34d
Antoni Luong Pham <Antoni.LuongPham@GMail.com>
2021-08-06 22:03:09 -0700

    add Numbers-from-1-to-100 program

---
+++ Antoni-Python-Projects/Numbers-from-1-to-100.py
@@ -0,0 +1,8 @@
+numbers_from_1_to_100 = []
+i = 0
+while i < 100:
+    i += 1
+    numbers_from_1_to_100.append(i)
+    print(i)
+
+print(f"i is {i} and numbers from 1 to 100 are {numbers_from_1_to_100}.")
