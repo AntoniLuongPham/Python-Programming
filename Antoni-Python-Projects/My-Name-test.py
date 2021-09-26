83d0dbfe46e86d6942451d483002a32a5af28bb4
Antoni Luong Pham <Antoni.LuongPham@gmail.com>
2021-09-25 19:54:18 -0700

    add my name test

---
+++ Antoni-Python-Projects/My-Name-test.py
@@ -0,0 +1,49 @@
+A = 'A'
+B = 'N'
+C = 'T'
+D = 'O'
+E = 'N'
+F = 'I'
+
+print("Antoni's my name.")
+my_name = input("What's my name?(all lowercase!) ")
+if my_name == 'antoni':
+    print('Right!')
+else:
+    print('Wrong')
+
+first_letter = input("What's my first letter in my name?(capital letter) ")
+if first_letter == A:
+    print('Right!')
+else:
+    print('Wrong')
+
+second_letter = input("What's my second letter in my name?(capital letter) ")
+if second_letter == B:
+    print('Right!')
+else:
+    print('Wrong')
+
+third_letter = input("What's my third letter in my name?(capital letter) ")
+if third_letter == C:
+    print('Right!')
+else:
+    print('Wrong')
+
+forth_letter = input("What's my forth letter in my name?(capital letter) ")
+if forth_letter == D:
+    print('Right!')
+else:
+    print('Wrong')
+
+fifth_letter = input("What's my fifth letter in my name?(capital letter) ")
+if fifth_letter == E:
+    print('Right!')
+else:
+    print('Wrong')
+
+sixth_letter = input("What's my sixth letter in my name?(capital letter) ")
+if sixth_letter == F:
+    print('Right!')
+else:
+    print('Wrong')
