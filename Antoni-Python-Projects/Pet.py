1fc65ee60e658039350f9f7eab9da7a181b375ea
Antoni Luong Pham <Antoni.LuongPham@gmail.com>
2021-10-03 15:36:46 -0700

    add Pet program

---
+++ Antoni-Python-Projects/Pet.py
@@ -0,0 +1,17 @@
+class Pet:
+    def __init__ (self, name: str, thing_to_do: str):
+        self.name = name
+        self.thing_to_do = thing_to_do
+
+    def do_something(self):
+        print(f'I am {self.thing_to_do}')
+
+
+cat = Pet('cat', 'climbing trees')
+cat.do_something()
+
+dog = Pet('dog', 'chasing cats')
+dog.do_something()
+
+bird = Pet('bird', 'flying')
+bird.do_something()
